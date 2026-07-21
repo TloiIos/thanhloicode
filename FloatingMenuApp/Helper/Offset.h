10e959210e364c4965fe83cdb91ed6aa9ef1730e
TloiIos <tctloi123@gmail.com>
2026-07-22 00:46:55 +0700

    Update Offset.h

--- FloatingMenuApp/Helper/Offset.h
+++ FloatingMenuApp/Helper/Offset.h
@@ -3,7 +3,6 @@
 
 // ==================== FREE FIRE OFFSETS ====================
 // Phiên bản: 1.105.1 (Cập nhật 2024)
-// ⚠️ Cập nhật khi game có bản vá mới
 
 // ==================== PLAYER OFFSETS ====================
 #define OFFSET_GET_HP               0x543592C
@@ -21,9 +20,6 @@
 #define OFFSET_PLAYER_ATTRIBUTES    0x708
 #define OFFSET_PLAYER_SPEED         0x260
 #define OFFSET_IS_BOT               0x438
-#define OFFSET_PLAYER_HEALTH        0x540
-#define OFFSET_PLAYER_SHIELD        0x544
-#define OFFSET_PLAYER_ARMOR         0x548
 
 // ==================== TRANSFORM OFFSETS ====================
 #define OFFSET_GET_TRANSFORM        0x91B82E4
@@ -36,15 +32,11 @@
 #define OFFSET_GET_CAMERA           0x915E9E4
 #define OFFSET_WORLD_TO_VIEWPORT    0x915E364
 #define OFFSET_CAMERA_TRANSFORM     0x380
-#define OFFSET_CAMERA_FOV           0x3A0
 
 // ==================== GAME OFFSETS ====================
 #define OFFSET_CURRENT_MATCH        0x55C4DA4
 #define OFFSET_GET_LOCAL_PLAYER     0x2FFE494
 #define OFFSET_PLAYERS_DICTIONARY   0x148
-#define OFFSET_GAME_STATE           0x150
-#define OFFSET_TEAM_MEMBERS         0x160
-#define OFFSET_ENEMY_COUNT          0x168
 
 // ==================== BONE OFFSETS ====================
 #define OFFSET_HEAD_POSITION        0x54547E0
@@ -57,8 +49,6 @@
 #define OFFSET_RIGHT_HAND           0x53C370C
 #define OFFSET_LEFT_FOREARM         0x53C3810
 #define OFFSET_RIGHT_FOREARM        0x53C3914
-#define OFFSET_LEFT_ELBOW           0x54552B8
-#define OFFSET_RIGHT_ELBOW          0x54553C4
 
 // ==================== WEAPON OFFSETS ====================
 #define OFFSET_GET_WEAPON           0x4DAEFBC
@@ -66,25 +56,5 @@
 #define OFFSET_SWAP_WEAPON          0x56E75DC
 #define OFFSET_TAKE_DAMAGE          0x4A82BFC
 #define OFFSET_RELOAD_WEAPON        0x4A8F238
-#define OFFSET_WEAPON_AMMO          0x5C0
-#define OFFSET_WEAPON_FIRERATE      0x5C4
-#define OFFSET_WEAPON_DAMAGE        0x5C8
-
-// ==================== VEHICLE OFFSETS ====================
-#define OFFSET_GET_VEHICLE          0x53E5A20
-#define OFFSET_VEHICLE_POSITION     0x300
-#define OFFSET_VEHICLE_SPEED        0x304
-#define OFFSET_VEHICLE_HEALTH       0x308
-
-// ==================== MISCELLANEOUS OFFSETS ====================
-#define OFFSET_RIGIDBODY            0x85D5EDC
-#define OFFSET_RIGIDBODY_VELOCITY   0x924E238
-#define OFFSET_RIGIDBODY_SET_VEL    0x924E300
-#define OFFSET_RIGIDBODY_GRAVITY    0x924E848
-#define OFFSET_RIGIDBODY_KINEMATIC  0x924E9B0
-
-#define OFFSET_ITRANSFORM           0x66F3DFC
-#define OFFSET_CATAPULT_FALLING     0x4A03040
-#define OFFSET_STOP_CATAPULT        0x4AD03D8
 
 #endif // OFFSETS_H
