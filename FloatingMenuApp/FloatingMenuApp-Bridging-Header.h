ca502a328866cc417a8c208e938d01a99d938852
TloiIos <tctloi123@gmail.com>
2026-07-21 23:09:08 +0700

    Refactor bridging header for clarity and formatting
    Removed unused import and added a newline at the end of the file.

--- FloatingMenuApp/FloatingMenuApp-Bridging-Header.h
+++ FloatingMenuApp/FloatingMenuApp-Bridging-Header.h
@@ -1,7 +1,8 @@
+cat > FloatingMenuApp/FloatingMenuApp-Bridging-Header.h << 'EOF'
 #ifndef FloatingMenuApp_Bridging_Header_h
 #define FloatingMenuApp_Bridging_Header_h
 
 #import "ESP/EspManager.h"
-#import "ESP/EspRenderView.h"
 
 #endif
+EOF
