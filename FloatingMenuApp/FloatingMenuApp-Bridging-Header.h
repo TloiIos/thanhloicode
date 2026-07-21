4a9ddda3715779cec205410bcf526a40a37becc8
TloiIos <tctloi123@gmail.com>
2026-07-21 23:11:53 +0700

    Update FloatingMenuApp-Bridging-Header.h

--- FloatingMenuApp/FloatingMenuApp-Bridging-Header.h
+++ FloatingMenuApp/FloatingMenuApp-Bridging-Header.h
@@ -1,8 +1,8 @@
-cat > FloatingMenuApp/FloatingMenuApp-Bridging-Header.h << 'EOF'
+
 #ifndef FloatingMenuApp_Bridging_Header_h
 #define FloatingMenuApp_Bridging_Header_h
 
 #import "ESP/EspManager.h"
 
 #endif
-EOF
+
