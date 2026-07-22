1b3ea3c4ee25251df17bf8d8213511be84258e40
TloiIos <tctloi123@gmail.com>
2026-07-22 10:09:20 +0700

    Refactor EspRenderView for singleton and rendering control
    Added sharedInstance method for singleton access and improved rendering logic.

--- FloatingMenuApp/ESP/EspRenderView.mm
+++ FloatingMenuApp/ESP/EspRenderView.mm
@@ -1,20 +1,30 @@
-
 #import "EspRenderView.h"
 #import "EspManager.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface EspRenderView () {
     CADisplayLink *_displayLink;
+    BOOL _isRendering;
 }
 @end
 
 @implementation EspRenderView
+
++ (instancetype)sharedInstance {
+    static EspRenderView *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[EspRenderView alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    });
+    return instance;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         self.userInteractionEnabled = NO;
+        self.windowLevel = UIWindowLevelStatusBar + 2;
         [self startRendering];
     }
     return self;
@@ -24,15 +34,23 @@
     if (_displayLink) return;
     _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(renderLoop)];
     [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    _isRendering = YES;
 }
 
 - (void)stopRendering {
     [_displayLink invalidate];
     _displayLink = nil;
+    _isRendering = NO;
 }
 
 - (void)renderLoop {
-    if (![EspManager isEspEnabled] && ![EspManager isAimbotEnabled]) return;
+    if (![EspManager isEspEnabled] && ![EspManager isAimbotEnabled]) {
+        if (_isRendering) {
+            self.hidden = YES;
+        }
+        return;
+    }
+    self.hidden = NO;
     [self setNeedsDisplay];
 }
 
